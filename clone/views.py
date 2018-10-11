d9b21095528481bcec0c400f93306ae87cc2234f
Sam Oluoch <samsoluoch@gmail.com>
2018-10-11 21:41:01 +0300

    defined the post method in the views.py that will enable users to make post requests using the api token

--- clone/views.py
+++ clone/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import MerchSerializer
+from rest_framework import status
 
 
 
@@ -107,6 +108,13 @@ class MerchList(APIView):
         all_merch_projects = Project.objects.all()
         serializers = MerchSerializer(all_merch_projects, many=True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = MerchSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
