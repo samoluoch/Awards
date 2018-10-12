b2927c6e28e10a265d46f78a52e8f88fe1eaea71
Sam Oluoch <samsoluoch@gmail.com>
2018-10-12 21:24:50 +0300

    created the form that will be used to capture the values of the ratings given by the judges

--- clone/forms.py
+++ clone/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile,Project
+from .models import Profile,Project,Rate
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text = 'Required')
@@ -20,3 +20,9 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        exclude = ['project', 'profile']
